e59d42d805eedb5314351c2477a9023b93467259
rajbhardwaj28 <rajraman8595@gmail.com>
2026-03-10 21:18:13 +0530

    Update salary_prediction_deployment.py

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -5,25 +5,25 @@ import joblib
 model = joblib.load("salary_prediction_rfr_model.pkl")
 encoder = joblib.load("label_encoder_Salary.pkl")
 
-st.title("Salary Prediction model")
+st.title("Salary Prediction Model")
 
 age =st.number_input("Enter your age", 18.65)
-gender = st.selectbox("Select your gender", encoder["Gender"].classes)
-education = st.selectbox("Select your education", encoder["Education"].classes)
-job_title = st.selectbox("Select your job title", encoder["Job_Title"].classes)
-experience = st.number_input("Enter your experience (in years)", 0.50)
+gender = st.selectbox("Select Your Gender", encoder["Gender"].classes_)
+education = st.selectbox("Select Your Education", encoder["Education Level"].classes_)
+job_title = st.selectbox("Select Your Job Title", encoder["Job_Title"].classes_)
+experience = st.number_input("Enter Your Experience (in years)", 0.50)
 
 df = pd.DataFrame({
     "Enter your age":[age],
-    "Select your gender":[gender],
-    "Select your education":[education],
-    "Select your job title":[job_title],
-    "Enter your experience (in years)":[experience]
+    "Select Your Gender":[gender],
+    "Select Your Education":[education],
+    "Select Your Job Title":[job_title],
+    "Enter Your Experience (in years)":[experience]
 })
 
-if st.button("predict salary"):
+if st.button("Predict Salary"):
   for col in encoder:
     df[col] = encoder[col].transform(df[col])
 
     prediction = model.predict(df)
-    st.success(f"predicted salary : {prediction[0]:}")
+    st.success(f"Predicted Salary : {prediction[0]:}")
